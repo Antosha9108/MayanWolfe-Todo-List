b70ff7a68de867ae5256281f6c105ddda924cf4b
Antosha9108 <shelkovnikov9108@gmail.com>
2022-07-27 14:06:19 -0400

    second try

---
+++ server.js
@@ -0,0 +1,90 @@
+//Declare variables
+const { request } = require('express');
+const express = require('express')
+const app = express()
+const PORT = 8400;
+const mongoose = require('mongoose')
+require('dotenv').config()
+//add model variable
+const TodoTask = require('./models/todotasks')
+
+//set middleware
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    {useNewUrlParser:true},
+    () => {console.log('Connected to db!')})
+        
+
+//GET METHOD
+    app.get("/", async (req, res) => {
+        try {
+            TodoTask.find({}, (err, tasks) => {
+                res.render("index.ejs", { todoTasks: tasks });
+            });
+        // console.log(todoTasks)
+        } catch (err) {
+            if (err) return res.status(500).send(err);
+        }
+    });
+
+//POST METHOD
+app.post('/',async (req,res)=> {
+    const todoTask = new TodoTask(
+        {
+            title: req.body.title, // we are pulling the title and content from the object from the database
+            content: req.body.content
+        }
+    )
+    try {
+        await todoTask.save()
+        console.log(todoTask)
+        res.redirect('/')
+    } catch(err) {
+        if (err) return res.status(500).send(err)
+        res.redirect('/')
+    }
+})
+
+
+//UPDATE OR EDIT METHOD
+app
+    .route('/edit/:id')
+    .get ((req,res) => {
+        const id = req.params.id
+        TodoTask.find({}, (err,tasks) => {
+            res.render('edit.ejs', {
+                todoTasks:tasks, idTask: id })
+            })
+        })
+    .post((req,res)=> {
+        const id = req.params.id
+        TodoTask.findByIdAndUpdate(
+            id,
+            {
+                title:req.body.title,
+                content:req.body.content
+            },
+            err => {
+                if (err) return res.status(500).send(err)
+                res.redirect('/')
+            }
+        )
+    })
+    
+// DELETE
+app
+    .route("/remove/:id")
+    .get((req,res) => {
+        const id = req.params.id
+        TodoTask.findByIdAndRemove(id,err => {
+            if (err) return res.status(500).send(err)
+            res.redirect('/')
+        })
+    })
+
+
+app.listen(PORT,() => console.log(`server is running on ${PORT}`))
